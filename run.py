dbbded42e9dae50aa01878486a212804f3bfee08
AvitalGalper <Danidin18dani>
2025-04-02 16:48:18 +0300

    שינוי הרצה

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
-from web3 import Web3
+from waitress import serve
+
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=80)
+    serve(app, host="0.0.0.0", port=80)
