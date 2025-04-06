bbae8eebde32cf1d7da1f30f45686c206ab6ce19
AvitalGalper <Danidin18dani>
2025-04-06 16:53:36 +0300

    עיצוב חדש

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
-# from app import create_app
-# from waitress import serve
-
-# app = create_app()
-
-# if __name__ == '__main__':
-#     serve(app, host="0.0.0.0", port=80)
-
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    serve(app, host="0.0.0.0", port=80)
+
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port=5000)
