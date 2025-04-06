27fd3a855762176705b14a5f8a59e41d16787957
AvitalGalper <Danidin18dani>
2025-04-06 15:01:32 +0300

    לוקלהוסט

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+# from app import create_app
+# from waitress import serve
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     serve(app, host="0.0.0.0", port=80)
+
 from app import create_app
-from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=80)
+    app.run(debug=True, host='127.0.0.1', port=5000)
