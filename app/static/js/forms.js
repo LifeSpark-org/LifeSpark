417c637a8035d11e90960a5ad82a570532f1cbcb
AvitalGalper <Danidin18dani>
2025-04-10 17:55:07 +0300

    הוספת אייקון לסיסמא

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -769,3 +769,19 @@ function showNotification(type, message) {
         }, 5000);
     }
 }
+
+// Password Visibility Toggle Function
+function togglePasswordVisibility(inputId) {
+    const passwordInput = document.getElementById(inputId);
+    const toggleIcon = passwordInput.parentElement.querySelector('.password-toggle');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye');
+    }
+}
