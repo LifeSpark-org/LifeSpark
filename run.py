6dce160dfaeb83f0ca09e5470a0b7afbe8f07b22
AvitalGalper <Danidin18dani>
2025-06-23 16:24:20 +0300

    תיקונים

--- run.py
+++ run.py
@@ -6,9 +6,3 @@ app = create_app()
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=80, threads=100)
 
-# from app import create_app
-
-# app = create_app()
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='127.0.0.1', port=5000)
